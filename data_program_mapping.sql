c246fb7c1532a11e1564ed70be2cb205ac5404e7
Adam Preston <adam.m.preston@gmail.com>
2019-06-27 10:16:27 -0600

    select only what we need from mapping and training

--- data_program_mapping.sql
+++ data_program_mapping.sql
@@ -1,3 +1,3 @@
-SELECT * from data_program_mapping
+SELECT data_program_mapping.* from data_program_mapping
 INNER JOIN data_files df ON data_program_mapping.data_files_id = df.data_files_id
 INNER JOIN submission_latest_vw ON submission_latest_vw.submission_id = df.submission_id;
--- data_program_training.sql
+++ data_program_training.sql
@@ -1,3 +1,3 @@
-SELECT * FROM data_program_training
+SELECT data_program_training.* FROM data_program_training
 INNER JOIN data_files df ON data_program_training.data_files_id = df.data_files_id
 INNER JOIN submission_latest_vw ON submission_latest_vw.submission_id = df.submission_id;
